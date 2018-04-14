96b0f1c3a86264d4ed7e75cd8ceeb9f27ca44452
greedjow <eric.diamonika@hetic.net>
2018-04-15 01:49:33 +0200

    ionic test: test modal

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+import { BasicPage } from '../modals/basic/page';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
@@ -10,7 +10,7 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = BasicPage;
   tab3Root = ContactPage;
 
   constructor() {
